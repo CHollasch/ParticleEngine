c451fae4d352fe7a080213d4e6a71ec7ee77c78b
Connor Hollasch <connor@hollasch.net>
2015-02-05 23:46:16 -0800

    Restart star from 0

--- src/me/hollasch/particles/simulations/stars/StarParticle.java
+++ src/me/hollasch/particles/simulations/stars/StarParticle.java
@@ -15,26 +15,13 @@ public class StarParticle extends Particle {
     private double frontY;
 
     private double direction = Math.random() * (Math.PI * 2);
-    private double speed = 1.1;
 
     public StarParticle(int x, int y) {
         super(x, y);
-
-        frontX = x + Math.cos(direction) * 30;
-        frontY = y + Math.sin(direction) * 30;
     }
 
     public void tick() {
-        double x = Math.cos(direction);
-        double y = Math.sin(direction);
 
-        speed*=1.01;
-
-        centerX += (x * speed);
-        centerY += (y * speed);
-
-        frontX += (x * speed);
-        frontY += (y * speed);
     }
 
     public void paint(ParticleSystem particleHost, Graphics g) {
